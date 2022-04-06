17905fbc27c9fcdd6993ec2b7f4a9b75d850ad7f
Seth Kitchen <skitchen@Hunter.com>
2022-04-06 14:52:45 -0500

    initial commit

---
+++ IntermediateDynamics/DynamicsVector3D.cs
@@ -0,0 +1,19 @@
+﻿using MathNet.Spatial.Euclidean;
+using System;
+using MathNet.Symbolics;
+using Expr = MathNet.Symbolics.SymbolicExpression;
+
+namespace IntermediateDynamics
+{
+    public static class DynamicsVector3D
+    {
+        public const string I_HAT_LATEX = @"\hat{\textbf{i}}";
+        public const string J_HAT_LATEX = @"\hat{\textbf{j}}";
+        public const string K_HAT_LATEX = @"\hat{\textbf{k}}";
+
+        public static string ComponentRepresentation(this Vector3D vector)
+        {
+            return vector.X + I_HAT_LATEX + "+" + vector.Y + J_HAT_LATEX + "+" + vector.Z + K_HAT_LATEX;
+        }
+    }
+}
---
+++ IntermediateDynamicsTestApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using MathNet.Spatial.Euclidean;
+using System;
+using IntermediateDynamics;
+
+namespace IntermediateDynamicsTestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String ans = (new Vector3D(2, 4, 5)).ComponentRepresentation();
+            Console.WriteLine(ans);
+        }
+    }
+}
