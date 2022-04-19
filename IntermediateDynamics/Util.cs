086c7c9a343c49e296e61a6dd1481638f6a243df
Seth Kitchen <sjkyv5@mst.edu>
2022-04-18 23:48:02 -0500

    add final ch 2 ex

--- IntermediateDynamics/Util.cs
+++ IntermediateDynamics/Util.cs
@@ -10,13 +10,13 @@ namespace IntermediateDynamics
 {
     public class Util
     {
-        public static double SolveForVariable(double lower_bound, double max_search_value, string variable, double rhsAns, Expr toSolve)
+        public static double SolveForVariable(double lower_bound, double max_search_value, string variable, double rhsAns, double margin, Expr toSolve)
         {
             for (double i = lower_bound + 0.001; i <= max_search_value; i += 0.001)
             {
                 var symbols = new Dictionary<string, FloatingPoint> { { variable, i } };
                 FloatingPoint ans = toSolve.Evaluate(symbols);
-                if (Math.Abs(ans.RealValue - rhsAns) < .01)
+                if (Math.Abs(ans.RealValue - rhsAns) < margin)
                 {
                     return i;
                 }
